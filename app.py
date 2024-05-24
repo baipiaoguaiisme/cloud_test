6f3a810c3f39c65609bf3e6b78cfa08ece17932e
baipiaoguaiisme <849875115@qq.com>
2024-05-25 02:12:12 +0800

    insert a route about /app

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/app')
+def app():
+    return "route app"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
